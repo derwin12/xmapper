6e6b92d99b5f76764f8826d8b54c257f4dd4f3a3
derwin12 <4643499+derwin12@users.noreply.github.com>
2025-02-20 16:34:19 -0500

    trees

--- xMapper/xMapper/classify.py
+++ xMapper/xMapper/classify.py
@@ -1,4 +1,3 @@
-
 from data_types import MODEL_TYPES
 import re
 
@@ -16,6 +15,7 @@ def fuzzy_match(key, name, display_as, description):
     # Check if the processed key matches any of the processed fields
     return key_processed in name_processed or key_processed in display_as_processed or key_processed in description_processed
 
+
 def classify_model(model, cache):
     name = model.get("name", "").lower()
     display_as = model.get("DisplayAs", "").lower()
@@ -23,26 +23,41 @@ def classify_model(model, cache):
     pixel_count = int(model.get("parm2", 0))
 
     print("Classify: ", model['name'] + " - " + model['DisplayAs'])
-    #if name in cache:
-     #   print('Found in cache ', model['name'], cache[name])
-      #  return cache[name]
+    # if name in cache:
+    #   print('Found in cache ', model['name'], cache[name])
+    #  return cache[name]
 
-# Handle Trees first
+    # Handle Trees first
     print(name, display_as)
-    if(display_as == 'tree 360'):
-        print("Found a 360 tree")
+    if (display_as == 'tree 360'):
+        if('TreeSpiralRotations' in model):
+            print("Found a spiral")
+            cache[name] = "T:Spiral_Tree"
+            return "T:Spiral_Tree"
+
+        if('parm2' in model):
+            if(int(model['parm2']) < 50):
+                cache[name] = "T:Tree"
+                return "T:Tree"
+
         cache[name] = "T:Mega_Tree"
         return "T:Mega_Tree"
 
-# Check for layers in an arch
+    # Check for layers in an arch
+    if (display_as == 'arches'):
+        if ('LayerSizes' in model):
+            values = [x.strip() for x in model["LayerSizes"].split(",")]
+            if (len(values) >= 2):
+                cache[name] = "T:TripleArch"
+                return "T:TripleArch"
 
-# Match based on name and model_type
+    # Match based on name and model_type
     for key, model_type in MODEL_TYPES.items():
         if fuzzy_match(key, name, display_as, description):
             cache[name] = model_type
             return model_type
-        #else:
-         #   print('Failed search', key, ' not found in ', name, display_as, description)
+        # else:
+        #   print('Failed search', key, ' not found in ', name, display_as, description)
 
     # Fuzzy Exact Match
     # Model Type
